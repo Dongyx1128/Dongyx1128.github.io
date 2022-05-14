86e05ed2558480656806195369e54de971a576d2
Dongyx1128 <597365773@qq.com>
2022-05-14 18:06:10 +0800

    Site updated: 2022-05-14 18:06:10

--- js/theme-setting.js
+++ js/theme-setting.js
@@ -5,18 +5,19 @@ function isNightFun() {
     var isNightTemp = localStorage.getExpire('night');
 
     // 第一次进来判断是白天还是晚上
-    // if (isNightTemp == null || isNightTemp == undefined) {
-        // if (isNightRange("19:00", "23:59") || isNightRange("00:00", "07:00")) {
-            // isNightTemp = 'true';
-        // } else {
-            // isNightTemp = 'false';
-        // }
-        // localStorage.setExpire("night", isNightTemp, expireTime1H);
-    // }
     if (isNightTemp == null || isNightTemp == undefined) {
-        isNightTemp = 'false';
+        if (isNightRange("22:00", "23:59") || isNightRange("00:00", "08:00")) {
+            isNightTemp = 'true';
+        } else {
+            isNightTemp = 'false';
+        }
         localStorage.setExpire("night", isNightTemp, expireTime1H);
     }
+    // 禁用深色模式，只使用亮色
+    // if (isNightTemp == null || isNightTemp == undefined) {
+    //     isNightTemp = 'false';
+    //     localStorage.setExpire("night", isNightTemp, expireTime1H);
+    // }
     return isNightTemp;
 }
 
